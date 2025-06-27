871676fba5cb03df98964993d6b97fcdede2e2aa
Lucas Oliveira <122385395+luzallenbates@users.noreply.github.com>
2025-06-27 18:45:28 -0300

    Update elev3.py

--- elev3.py
+++ elev3.py
@@ -4,4 +4,21 @@ i=0
 k=0
 j=0
 for i in range(11)
-  
+  subiu = False
+  desceu = False
+  if subida[k] == i:
+    subiu = True
+  if descida[k] == i:
+    desceu = True
+
+  if subiu and desceu:
+    print(f"{i} º andar subiu e desceu")
+  elif subiu
+    print(f"{i} º andar subiu")
+elif desceu:
+    for j in range(len(descida))
+      if descida[j]>=subida and descida[j] < i+1 #essa parte tem que estar na verificação do true e false
+      print(f"{i} º andar desceu") #essa vai ser a condição caso desceu==True
+      elif:
+      #teria que ser um brake ou adição no j ou algo que pulasse para a próxima etapa
+      
