058cc9e44e43f8ea5dab009754092fcf81db9b6b
Lucas Oliveira <122385395+luzallenbates@users.noreply.github.com>
2025-07-05 20:20:18 -0300

    Add files via upload
    calculadora de juros compostos/depreciação

---
+++ contas (cópia 1).py
@@ -0,0 +1,12 @@
+print("Matemática da Diana\n");
+sn = "s"
+while sn == "s":
+    valor= float(input("digite o valor:"))
+    coefmu= float(input("digite o CM: "))
+    expoente=int(input("digite os meses:"))
+    valor2 = float (valor * (coefmu ** expoente))
+    print("O resultado é:")
+    print(valor2)
+
+    sn= input("deseja continuar? (s/n) \n")
+print("acabouuu") 
